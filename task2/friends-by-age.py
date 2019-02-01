3dca79e96c598564345d3f10dd3cb4af877eb078
Prathik Pai <prathik@logichive.in>
2019-02-01 09:43:30 +0530

    Update friends-by-age.py

--- task2/friends-by-age.py
+++ task2/friends-by-age.py
@@ -11,8 +11,16 @@ def parseLine(line):
 
 lines = sc.textFile("file:///SparkCourse/fakefriends.csv")
 rdd = lines.map(parseLine)
-totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
+totalsByAge = rdd.mapValues(lambda x: (x, 1))
+print ()
+result=totalsByAge.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+averagesByAge = result.mapValues(lambda x: x[0] / x[1])
 results = averagesByAge.collect()
 for result in results:
     print(result)
+for x,y in results:
+	print ("age:",x," Avg Friends:",y)
+
+
+
+
